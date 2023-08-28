f767427f30dd759a01143eba7088a1d29ac2c274
melonheader <artem.baranowsky@gmail.com>
2023-08-28 18:49:36 +0200

    Added connectivity visualisation via graph

--- playground/test_assessNetwork.R
+++ playground/test_assessNetwork.R
@@ -27,8 +27,6 @@ addmats <- function() {
 
 buildnet <- function(l_processed) {
 
-
-
 }
 
 
@@ -121,7 +119,130 @@ assemble.cons.nodes <- function(l_processed, angle_type, fringe = 3) { #nolint
 l_cons_nodes <- assemble.cons.nodes(l_processed, c("sharp", "blunt"))
 
 
-build.net <- function(l_cons_nodes,
-                      angle_type = "sharp") {
 
+## After sleeping on the problem, can we first check relationship between
+## colsum and rowsums?
+names(l_cons_nodes)
+
+assemble.onehotmat <- function(cons_nodes) { #nolint
+  to_keep <- str_detect(names(cons_nodes), "conserved", negate = TRUE)
+  purrr::map2(
+    cons_nodes[to_keep],
+    names(cons_nodes[to_keep]),
+    function(x, y) {
+      tmp <- quote(y)
+      tmp <- x[, eval(tmp) := 1]
+      return(tmp)
+    }
+  ) %>%
+    purrr::reduce(
+      .,
+      function(x, y) {
+        merge(x, y, all = TRUE, by = "edge")
+      }
+    ) -> ohm
+  ohm[is.na(ohm)] <- 0
+  return(ohm)
 }
+
+ohm_sharp <- assemble.onehotmat(l_cons_nodes$sharp)
+ohm_blunt <- assemble.onehotmat(l_cons_nodes$blunt)
+
+## what if... bind ohms and then walk from the sample with highest number
+## of critical angles down to the sample with the lowest.
+# Our rationale is that the largest number of critical angles ensues
+# the biggest connectivity to other samples.
+tst_tib <- tibble(
+  shcsums = colSums(ohm_sharp[, 2:14]),
+  blcsums = colSums(ohm_blunt[, 2:14])
+)
+ggplot(data = tst_tib) + geom_point(aes(x = shcsums, y = blcsums), size = 2)
+
+assemble.conmat <- function(ohm) { #nolint
+  idcs <- colnames(ohm[, 2:dim(ohm)[2]])
+  ## arrange descending by the total of critical angles
+  ord <- order(colSums(ohm[, ..idcs]), decreasing = TRUE)
+  idcs <- idcs[ord]
+  purrr::map_dfr(
+    idcs,
+    function(idx) {
+      tmp <- ohm[, ..idcs]
+      tmp <- tmp[tmp[[idx]] > 0, ]
+      out <- colSums(tmp)
+      return(out)
+    }
+  ) %>% as.matrix(.) -> m_ass
+  rownames(m_ass) <- colnames(m_ass)
+  return(m_ass)
+}
+
+
+## We can sum up both sharp and blunt angles and use the resulting
+## matrix as a similarity matrix
+l_conm <- list()
+l_conm$sharp <- assemble.conmat(ohm_sharp)
+l_conm$blunt <- assemble.conmat(ohm_blunt)
+conm_summed <- (l_conm$sharp + l_conm$blunt)
+
+# -----------------------------------------------------------------------
+## what about hierarchical clustering?
+hclust.conmat <- function(conm) { #nolint
+  hclust(as.dist(1 - apply(conm, 2, function(x) x / diag(conm))))
+}
+
+(l_conm$sharp + l_conm$blunt) %>% hclust.conmat() %>% plot()
+
+# -----------------------------------------------------------------------
+## and about graph?
+conm.to.tgrph <- function(conm) {
+  ##
+  conm <- apply(conm, 2, function(x) x / diag(conm))
+  ##
+  tgrph_nodes <- data.frame(name = colnames(conm))
+  node_id <- seq_along(tgrph_nodes$name)
+  ##
+  colnames(conm) <- node_id
+  rownames(conm) <- node_id
+  ##
+  conm[upper.tri(conm, diag = TRUE)] <- NA
+  tgrph_edges <- data.table::as.data.table(conm, keep.rownames = "x")
+  tgrph_edges <- data.table::melt(tgrph_edges,
+                            id.vars = "x",
+                            variable.name = "to",
+                            value.name    = "weight")
+  tgrph_edges <- tgrph_edges[!is.na(weight)]
+  colnames(tgrph_edges) <- c("from", "to", "weight")
+  tgrph_edges <- as.data.frame(apply(tgrph_edges, 2, as.numeric))
+  ##
+  tidygraph::tbl_graph(
+    nodes = tgrph_nodes,
+    edges = tgrph_edges,
+    directed = FALSE
+  )
+}
+
+tgrph_summed <- conm.to.tgrph(conm_summed)
+library(tidygraph)
+library(ggraph)
+ggraph(tgrph_summed, layout = 'lgl') +
+  geom_edge_link(
+    aes(
+      color = weight,
+      width = weight
+    )
+   ) + 
+  ggraph::scale_edge_width_continuous(
+    breaks = seq(0, 1, by = 0.1),
+    range = c(1, 3)
+  ) +
+  ggraph::scale_edge_colour_gradientn(colors = viridis::inferno(n = 20)) +
+  geom_node_point(size = 8) + 
+  geom_node_label(aes(label = name), repel = TRUE) +
+  theme(legend.position = 'bottom')
+
+
+## Conclusion
+# Graphs and hclust trees provide a great visualisation for the connectivity
+# between the datasets, yet it still doesn't solve the problem with 
+# angle selection
+save.image(file = "./snapshots/ENV_test_assessNetwork.RData")
