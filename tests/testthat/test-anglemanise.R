a6350253183ef9a55a9dcaabedf279bc980a19c9
kollo97 <aaron.kollo@gmail.com>
2024-02-08 15:50:44 +0100

    added first unit tests

--- tests/testthat/test-anglemanise.R
+++ tests/testthat/test-anglemanise.R
@@ -1,41 +1,89 @@
-# test_that("input validation works correctly", {
-#   expect_error(anglemanise(NULL), "seurat_list must be a list")
-#   expect_error(anglemanise(list(), n_threads = "non-numeric"), "n_threads must be numeric")
-#   expect_error(anglemanise(list(), n_threads = -1), "n_threads must be positive")
-#   expect_error(anglemanise(list(), path_to_write_angles = 123), "path_to_write_angles must be a character string")
-#   expect_error(anglemanise(list(), extrema = c(0.001, 0.002, 0.003)), "extrema must have length 1 or 2")
-# })
-# 
-# 
-# test_that("anglemanise returns expected structure", {
-#   # Assuming you have a predefined 'seurat_list' for testing
-#   result <- anglemanise(seurat_list, extrema = 0.001, n_threads = 2, path_to_write_angles = tempdir())
-#   
-#   expect_type(result, "list")
-#   expect_true(all(c("x_sharp", "x_blunt", "l_angles", "data_info") %in% names(result)))
-#   expect_true(is(result$x_sharp, "dgCMatrix")) #change to expect_type()
-#   expect_true(is(result$x_blunt, "dgCMatrix"))
-#   # Add more checks as necessary
-# })
-# 
-# 
-# test_that("anglemanise creates directory if non-existent", {
-#   non_existent_path <- file.path(tempdir(), "new_dir")
-#   expect_false(dir.exists(non_existent_path))
-#   
-#   anglemanise(seurat_list, extrema = 0.001, n_threads = 1, path_to_write_angles = non_existent_path)
-#   expect_true(dir.exists(non_existent_path))
-# })
-# 
-# 
-# test_that("anglemanise writes files to specified directory", {
-#   test_path <- file.path(tempdir(), "test_files")
-#   dir.create(test_path)
-#   
-#   anglemanise(seurat_list, extrema = 0.001, n_threads = 1, path_to_write_angles = test_path)
-#   
-#   # Check if files are created. This will depend on how your function writes files
-#   # Example:
-#   # expect_true(length(list.files(test_path)) > 0)
-# })
-# 
+context("anglemanise function tests")
+
+test_that("Input validation works correctly", {
+  # Invalid n_threads
+  expect_error(anglemanise(sl_scaled_test_data, n_threads = -1))
+  
+  # Invalid path_to_write_angles
+  expect_error(anglemanise(sl_scaled_test_data, path_to_write_angles = 123))
+})
+
+
+result <- anglemanise(sl_scaled_test_data)
+
+test_that("Function handles valid inputs correctly", {
+  # Verify the structure of the result
+  expect_type(result, list)
+  expect_equal(length(result), 4)
+  expect_true("x_sharp" %in% names(result))
+  expect_true("x_blunt" %in% names(result))
+  expect_true("data_info" %in% names(result))
+  expect_true("l_angles" %in% names(result))
+  
+})
+
+test_that("Sharp and Blunt matrices are consistent", {
+  sharp_data <- matrix(c(
+    5, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+    0, 5, 1, 0, 0, 0, 0, 0, 0, 2,
+    0, 1, 5, 2, 0, 2, 0, 0, 0, 3,
+    1, 0, 2, 5, 1, 1, 1, 0, 1, 1,
+    0, 0, 0, 1, 5, 0, 1, 1, 0, 1,
+    1, 0, 2, 1, 0, 5, 1, 0, 0, 1,
+    0, 0, 0, 1, 1, 1, 5, 1, 0, 0,
+    1, 0, 0, 0, 1, 0, 1, 5, 0, 1,
+    0, 0, 0, 1, 0, 0, 0, 0, 5, 0,
+    1, 2, 3, 1, 1, 1, 0, 1, 0, 5),
+    nrow = 10, byrow = TRUE)
+  sharp_sparse_matrix <- as(data, "dgCMatrix")
+  sharp_sparse_matrix@Dimnames[[2]] <- c("AL669831.5", "NOC2L", "HES4", "ISG15",
+                                         "SDF4", "UBE2J2", "ACAP3", "INTS11",
+                                         "AURKAIP1", "CCNL2")
+  
+  blunt_data <- matrix(c(
+    0, 0, 1, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 2, 0, 1, 1, 1, 0,
+    1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 2, 0, 0, 0, 0, 1, 0, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    0, 1, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 1, 0, 0, 0, 0),
+    nrow = 10, byrow = TRUE)
+  blunt_sparse_matrix <- as(data, "dgCMatrix")
+  blunt_sparse_matrix@Dimnames[[2]] <- c("AL669831.5", "NOC2L", "HES4", "ISG15",
+                                         "SDF4", "UBE2J2", "ACAP3", "INTS11", 
+                                         "AURKAIP1", "CCNL2")
+  
+  first_100_elements_sharp <- result$x_sharp[1:10,1:10]
+  first_100_elements_blunt <- result$x_blunt[1:10,1:10]
+  
+  expect_snapshot(
+    waldo::compare(first_100_elements_sharp, sharp_sparse_matrix)
+  )
+  expect_snapshot(
+    waldo::compare(first_100_elements_blunt, sharp_sparse_matrix_blunt)
+  )
+  })
+
+
+test_that("Check anglemanised $angles_dist", {
+  # Verify the structure of the result
+  expect_type(result, list)
+  expect_equal(length(result), 4)
+  expect_true("x_sharp" %in% names(result))
+  expect_true("x_blunt" %in% names(result))
+  expect_true("data_info" %in% names(result))
+  expect_true("l_angles" %in% names(result))
+  
+})
+
+
+test_that("Function handles edge cases correctly", {
+  # Edge cases like empty Seurat objects, objects with no scaled data, etc.
+  mock_seurat_list <-  list("testtest", "asd")
+  expect_error(anglemanise(mock_seurat_list))
+})
+
--- tests/testthat/test-import_sl.R
+++ tests/testthat/test-import_sl.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Objects with fewer cells than min_mcells are removed", {
+  sl <- sl_test_data
+  result <- import_sl(sl, min_mcells = 6)
+  expect_equal(length(result), 5)
+})
+
+test_that("SCTransform is applied if scaled data is not present", {
+  sl <- sl_test_data
+  result <- import_sl(sl, min_mcells = 6)
+  all_true <- all(sapply(result, function(x) "scale.data" %in% SeuratObject::Layers(x)))
+  expect_equal(all_true, TRUE)
+})
+
+# Tests for input validation
+test_that("Error is thrown for list with fewer than two Seurat objects", {
+  sl <- sl_test_data
+  expect_error(import_sl(sl, min_mcells = 6))
+})
+
+test_that("Error is thrown for non-Seurat objects in the list", {
+  mock_seurat_list <- list("testtest", "asd")
+  expect_error(import_sl(mock_seurat_list, min_mcells = 6))
+})
+
+test_that("Error is thrown for invalid min_mcells values", {
+  sl <- sl_test_data
+  expect_error(import_sl(sl, min_mcells = 5))
+  expect_error(import_sl(sl, min_mcells = "invalid"))
 })
